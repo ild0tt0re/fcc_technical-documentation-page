e50ef7a86ca64ca4f699ff6e462092d48159add5
ild0tt0re <ing.annunziata@gmail.com>
2019-11-08 20:37:55 +0100

    add intersection Web Api to highlight menu voices on scroll

--- src/main.js
+++ src/main.js
@@ -1,10 +1,44 @@
 var $switchThemeBtn;
+var observer;
+var targets;
 
 document.addEventListener('DOMContentLoaded', () => {
-    $switchThemeBtn = document.getElementById('switch-theme');
-    $switchThemeBtn.addEventListener('click', switchTheme);
+    initSwitchTheme()
+    initIntersectionApi();
 });
 
-function switchTheme() {
+function initSwitchTheme() {
+    $switchThemeBtn = document.getElementById('switch-theme');
+    $switchThemeBtn.addEventListener('click', _switchTheme);
+}
+
+function initIntersectionApi() {
+
+    var options = {
+        rootMargin: '0px',
+        threshold: [0.1]
+    }
+
+    var callback = (entries, observer) => {
+        entries.forEach(entry => {
+            var $section = document.getElementById(entry.target.id + "-menu-item");
+            if ($section && entry.isIntersecting) {
+                $section.style.fontWeight = 'bold';
+            }
+
+            if ($section && !entry.isIntersecting) {
+                $section.style.fontWeight = '100';
+            }
+
+        });
+    };
+
+    observer = new IntersectionObserver(callback, options);
+
+    targets = document.getElementsByClassName('main-section');
+    Array.from(targets).forEach(target => observer.observe(target));
+}
+
+function _switchTheme() {
     document.body.classList.toggle("dark");
 }
